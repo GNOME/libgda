00f0cd0a69d6aa14bf3a03131575a85ef134736c
Rodrigo Moya <rodrigo@linuxave.net>
2000-10-20 23:18:24 +0000

    added missing _() and N_() declarations which were missing after removing
    2000-10-21	Rodrigo Moya <rodrigo@linuxave.net>
    
    	* added missing _() and N_() declarations which were missing after
    	removing the gnome.h #includes. This has been the first GNOME-independent
    	successful compilation

--- lib/gda-client/gda-error.c
+++ lib/gda-client/gda-error.c
@@ -1,4 +1,4 @@
-/* GNOME DB libary
+/* GDA client library
  * Copyright (C) 1998,1999 Michael Lausch
  * Copyright (C) 1999 Rodrigo Moya
  *
@@ -17,8 +17,24 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include "config.h"
 #include "gda.h"
 #include "gda-error.h"
+
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  define _(String) gettext (String)
+#  define N_(String) (String)
+#else
+/* Stubs that do something close enough. */
+#  define textdomain(String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,Message) (Message)
+#  define dcgettext(Domain,Message,Type) (Message)
+#  define bindtextdomain(Domain,Directory)
+#  define _(String) (String)
+#  define N_(String) (String)
+#endif
 
 enum
 {
