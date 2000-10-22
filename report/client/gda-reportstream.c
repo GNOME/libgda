2ee7e4cc12c2a8e9e926048161e97a185d4d6f74
Carlos Perelló Marín <lxhispano@src.gnome.org>
2000-10-22 20:13:09 +0000

    Updated some report client objects and modified to the new GObject model

--- report/client/gda-reportstream.c
+++ report/client/gda-reportstream.c
@@ -3,16 +3,16 @@
  * Copyright (C) 2000 Carlos Perelló Marín <carlos@hispalinux.es>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  it under the terms of the GNU Library General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Library General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
@@ -27,10 +27,41 @@ enum
 static gint
 gda_reportstream_signals[LAST_SIGNAL] = {0, };
 
+
+#ifdef HAVE_GOBJECT
+static void gda_reportstream_finalize (GObject *object);
+#endif
+
 static void    gda_reportstream_class_init    (Gda_ReportStreamClass* klass);
 static void    gda_reportstream_init          (Gda_ReportStream* rs);
 
-guint
+#ifdef HAVE_GOBJECT
+GType
+gda_reportstream_get_type (void)
+{
+  static GType type = 0;
+
+  if (!type)
+    {
+      GTypeInfo info =
+      {
+       sizeof (Gda_ReportStreamClass),               /* class_size */
+        NULL,                                        /* base_init */
+        NULL,                                        /* base_finalize */
+       (GClassInitFunc) gda_reportstream_class_init, /* class_init */
+        NULL,                                        /* class_finalize */
+        NULL,                                        /* class_data */
+       sizeof (Gda_ReportStream),                    /* instance_size */
+        0,                                           /* n_preallocs */
+       (GInstanceInitFunc) gda_reportstream_init,    /* instance_init */
+        NULL,                                        /* value_table */
+      };
+      type = g_type_register_static (G_TYPE_OBJECT, "Gda_ReportStream", &info);
+    }
+  return type;
+}
+#else
+GtkType
 gda_reportstream_get_type (void)
 {
   static guint gda_reportstream_type = 0;
@@ -52,20 +83,33 @@ gda_reportstream_get_type (void)
     }
   return gda_reportstream_type;
 }
+#endif
 
+#ifdef HAVE_GOBJECT
+static void
+gda_reportstream_class_init (Gda_ReportStreamClass *klass, gpointer data)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
+  object_class->finalize = &gda_reportstream_finalize;
+  klass->parent = g_type_class_peek_parent (klass);
+}
+#else
 static void
 gda_reportstream_class_init (Gda_ReportStreamClass* klass)
 {
-  GtkObjectClass*   object_class;
-
-  object_class = (GtkObjectClass*) klass;
+  GtkObjectClass*   object_class = GTK_OBJECT_CLASS(klass);
   
   gtk_object_class_add_signals(object_class, gda_reportstream_signals, LAST_SIGNAL);
 }
+#endif
 
 static void
-gda_reportstream_init (Gda_ReportStream* rs)
+#ifdef HAVE_GOBJECT
+gda_reportstream_init (Gda_ReportStream* object, Gda_ReportStreamClass *klass)
+#else
+gda_reportstream_init (Gda_ReportStream *object)
+#endif
 {
-	g_return_if_fail(GDA_REPORTSTREAM_IS_OBJECT(rs));
+	g_return_if_fail(GDA_REPORTSTREAM_IS_OBJECT(object));
 }
--- report/client/gda-reportstream.h
+++ report/client/gda-reportstream.h
@@ -3,22 +3,32 @@
  * Copyright (C) 2000 Carlos Perelló Marín <carlos@hispalinux.es>
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  it under the terms of the GNU Library General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Library General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #ifndef __gda_reportstream_h__
-#define __gda_reportstream_h__ 1
+#define __gda_reportstream_h__
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#ifdef HAVE_GOBJECT
+#  include <glib-object.h>
+#else
+#  include <gtk/gtk.h>
+#endif
 
 /* Data Structures and Prototypes for the LibGDA Report Client
  * Library
@@ -26,37 +36,57 @@
 typedef struct _Gda_ReportStream       Gda_ReportStream;
 typedef struct _Gda_ReportStreamClass  Gda_ReportStreamClass;
 
-
 #define GDA_TYPE_REPORTSTREAM          (gda_reportstream_get_type())
+#ifdef HAVE_GOBJECT
+#  define GDA_REPORTSTREAM(obj) \
+          G_TYPE_CHECK_INSTANCE_CAST (obj, GDA_TYPE_REPORTSTREAM, Gda_ReportStream)
+#  define GDA_REPORTSTREAM_CLASS(klass) \
+          G_TYPE_CHECK_CLASS_CAST (klass, GDA_TYPE_REPORTSTREAM, Gda_ReportStreamClass)
+#  define GDA_IS_REPORTSTREAM(obj) \
+          G_TYPE_CHECK_INSTANCE_TYPE (obj, GDA_TYPE_REPORTSTREAM)
+#  define GDA_IS_REPORTSTREAM_CLASS(klass) \
+          GTK_CHECK_CLASS_TYPE ((klass), GDA_TYPE_REPORTSTREAM)
+#else
 #define GDA_REPORTSTREAM(obj)          GTK_CHECK_CAST(obj, GDA_TYPE_REPORTSTREAM, Gda_ReportStream)
 #define GDA_REPORTSTREAM_CLASS(klass)  GTK_CHECK_CLASS_CAST(klass, GDA_TYPE_REPORTSTREAM, GdaReportStreamClass)
-#define IS_GDA_REPORTSTREAM(obj)       GTK_CHECK_TYPE(obj, GDA_TYPE_REPORTSTREAM)
-#define IS_GDA_REPORTSTREAM_CLASS(klass) (GTK_CHECK_CLASS_TYPE((klass), GDA_TYPE_REPORTSTREAM))
+#define GDA_IS_REPORTSTREAM(obj)       GTK_CHECK_TYPE(obj, GDA_TYPE_REPORTSTREAM)
+#define GDA_IS_REPORTSTREAM_CLASS(klass) (GTK_CHECK_CLASS_TYPE((klass), GDA_TYPE_REPORTSTREAM))
+#endif
 
 struct _Gda_ReportStream
 {
-  GtkObject     object;
+#ifdef HAVE_GOBJECT
+  GObject         object;
+#else
+  GtkObject       object;
+#endif
   CORBA_Object  reportstream;
   CORBA_ORB     orb;
-/*  GList*        commands;
-  GList*        recordsets;
-  gchar*        provider;
-  gchar*        default_db;
-  gchar*        database;
-  gchar*        user;
-  gchar*        passwd;
-  GList*        errors_head;
-  gint          is_open;*/
 };
 
 struct _Gda_ReportStreamClass
 {
+#ifdef HAVE_GOBJECT
+  GObjectClass  parent_class;
+  GObjectClass *parent;
+#else
   GtkObjectClass parent_class;
+#endif
+/*  void (* warning) (Gda_ReportStream *object, const char *msg);
+  void (* error)   (Gda_ReportStream *object, const char *msg);*/
 };
 
-  
-guint              gda_reportstream_get_type            (void);
+#ifdef HAVE_GOBJECT
+GType        gda_reportstream_get_type      (void);
+#else
+GtkType      gda_reportstream_get_type      (void);
+#endif
+
 Gda_ReportStream*  gda_reportstream_new                 (CORBA_ORB orb);
-void               gda_reportstream_free                (Gda_ReportStream* cnc);
+void               gda_reportstream_free                (Gda_ReportStream* object);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
