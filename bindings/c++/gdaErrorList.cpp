91e4689e68ac209551bed9cc7dfe1d88ea4858d6
Reinhard Müller <reinhard@src.gnome.org>
2000-11-05 19:12:37 +0000

    Added #include "config.h" to all .cpp files

--- bindings/c++/gdaErrorList.cpp
+++ bindings/c++/gdaErrorList.cpp
@@ -16,6 +16,7 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include "config.h"
 #include "gdaErrorList.h"
 
 // Okay, this class doesn't exist in the C version - it is a list of errors
