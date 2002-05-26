866e3f85e6026a88ae99e99cd8c4f0a778f7383d
Carlos Perelló Marín <carlos@src.gnome.org>
2002-05-26 01:05:21 +0000

    Added copyright message and some fixes

--- report/libgda-report/gda-report-types.h
+++ report/libgda-report/gda-report-types.h
@@ -1,3 +1,28 @@
+/* GDA report libary
+ * Copyright (C) 2002 The GNOME Foundation.
+ *
+ * AUTHORS:
+ *	Carlos Perelló Marín <carlos@gnome-db.org>
+ *
+ * This Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#if !defined(__gda_report_types_h__)
+#  define __gda_report_types_h__
+
 typedef enum {
         GDA_REPORT_STYLE_FORM,
 	GDA_REPORT_STYLE_LIST
@@ -64,3 +89,5 @@ typedef struct {
 	guint8 blue;
 	guint8 yellow;
 } GdaReportColor;
+
+#endif
