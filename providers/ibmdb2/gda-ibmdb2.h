525bd18c08ee2bc72f27e0381020bee7131883cc
Holger Thon <holger@src.gnome.org>
2002-08-14 10:16:34 +0000

    - 2002-08-14 Holger Thon <holger.thon@gnome-db.org> 	- Added skeleton files for ibmdb2 provider (forgot these files)

---
+++ providers/ibmdb2/gda-ibmdb2.h
@@ -0,0 +1,42 @@
+/* GDA IBMDB2 Provider
+ * Copyright (C) 2002 The GNOME Foundation
+ *
+ * AUTHORS: Holger Thon <holger.thon@gnome-db.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#if !defined(__gda_ibmdb2_h__)
+#  define __gda_ibmdb2_h__
+
+#if defined(HAVE_CONFIG_H)
+#  include <config.h>
+#endif
+
+#include <glib/gmacros.h>
+#include <libgda/gda-intl.h>
+#include <libgda/gda-server-provider.h>
+#include <sqlcli1.h>
+#include "gda-ibmdb2-provider.h"
+//#include "gda-ibmdb2-recordset.h"
+
+#define GDA_IBMDB2_PROVIDER_ID          "GDA IBMDB2 provider"
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif
+
---
+++ providers/ibmdb2/libmain.c
@@ -0,0 +1,62 @@
+/* GDA IBMDB2 Provider
+ * Copyright (C) 2002 The GNOME Foundation
+ *
+ * AUTHORS:
+ *         Holger Thon <holger.thon@gnome-db.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <libgda/gda-intl.h>
+#include "gda-ibmdb2-provider.h"
+
+const gchar *plugin_get_name (void);
+const gchar *plugin_get_description (void);
+GList *plugin_get_connection_params (void);
+GdaServerProvider *plugin_create_provider (void);
+
+const gchar *
+plugin_get_name (void)
+{
+	return "IBMDB2";
+}
+
+const gchar *
+plugin_get_description (void)
+{
+	return _("GDA provider for IBMDB2");
+}
+
+GList *
+plugin_get_connection_params (void)
+{
+	GList *list = NULL;
+
+	list = g_list_append (list, g_strdup ("DATABASE"));
+	list = g_list_append (list, g_strdup ("HOST"));
+	list = g_list_append (list, g_strdup ("HOSTADDR"));
+	list = g_list_append (list, g_strdup ("OPTIONS"));
+	list = g_list_append (list, g_strdup ("PASSWORD"));
+	list = g_list_append (list, g_strdup ("PORT"));
+	list = g_list_append (list, g_strdup ("USER"));
+
+	return list;
+}
+
+GdaServerProvider *
+plugin_create_provider (void)
+{
+	return gda_ibmdb2_provider_new ();
+}
