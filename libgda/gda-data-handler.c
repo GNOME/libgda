2ff8288ea5cc14f2ed478396407b6e3827c6f008
Murray Cumming <murrayc@murrayc.com>
2007-02-08 09:10:50 +0000

    Make GObject a prerequisite, so instances of this interface can be used as
    2007-02-08  Murray Cumming  <murrayc@murrayc.com>
    
            * libgda/gda-data-handler.c: (gda_data_handler_get_type): Make GObject a prerequisite, so 
            instances of this interface can be used as object properties in libgnomedb.
            Actually, I wonder how an interface could be implemented without GObject.
    
    svn path=/trunk/; revision=2814

--- libgda/gda-data-handler.c
+++ libgda/gda-data-handler.c
@@ -41,6 +41,7 @@ gda_data_handler_get_type (void)
 		};
 		
 		type = g_type_register_static (G_TYPE_INTERFACE, "GdaDataHandler", &info, 0);
+                g_type_interface_add_prerequisite (type, G_TYPE_OBJECT);
 	}
 	return type;
 }
