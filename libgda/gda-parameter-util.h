8e013a55a563e5c368cfbc280e849f63e28350fe
Murray Cumming <murrayc@murrayc.com>
2007-04-03 13:01:36 +0000

    Clarify that the library and providers are under LGPL. It is just the
    2007-04-03  Murray Cumming  <murrayc@murrayc.com>
    
            * COPYING:
            * README: Clarify that the library and providers are under LGPL. It is just the 
            executables that are under GPL.
            * providers/postgres/gda-postgres-handler-bin.c:
            * providers/postgres/gda-postgres-handler-bin.h:
            * providers/sqlite/gda-sqlite-handler-bin.c:
            * providers/sqlite/gda-sqlite-handler-bin.h: Correct inconsistent license headers.
    
    svn path=/trunk/; revision=2887

--- libgda/gda-parameter-util.h
+++ libgda/gda-parameter-util.h
@@ -16,7 +16,6 @@
  * License along with this Library; see the file COPYING.LIB.  If not,
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- */SA
  */
 
 
