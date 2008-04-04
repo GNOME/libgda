29d92f68345889192542445da0f81c86cfee5254
Murray Cumming <murrayc@murrayc.com>
2008-04-04 13:48:56 +0000

    Prevent an access outside of an array, as found by valgrind.
    2008-04-04  Murray Cumming  <murrayc@murrayc.com>
    
    * providers/postgres/utils.c (gda_postgres_type_oid_to_gda):
    Prevent an access outside of an array, as found by valgrind.
    
    svn path=/branches/release-3-0-branch/; revision=3110

--- providers/postgres/utils.c
+++ providers/postgres/utils.c
@@ -121,6 +121,14 @@ gda_postgres_type_oid_to_gda (GdaPostgresTypeOid *type_data, gint ntypes, Oid po
 		if (type_data[i].oid == postgres_type) 
 			break;
 
+	/* Avoid accessing position -1: */
+	if(ntypes == 0)
+		return G_TYPE_STRING;
+
+	/* Avoid accesing beyone the array: */ 
+	if(i >= ntypes)
+		i = ntypes -1;
+       
   	if (type_data[i].oid != postgres_type)
 		return G_TYPE_STRING;
 
