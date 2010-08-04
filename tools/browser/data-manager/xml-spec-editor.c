08550a78da4f8acc5580c342dcd15c8fb7deb4e0
Vivien Malerba <malerba@gnome-db.org>
2010-08-04 20:03:36 +0200

    Removed GTK_CHECK_VERSION from merge with master

--- tools/browser/data-manager/xml-spec-editor.c
+++ tools/browser/data-manager/xml-spec-editor.c
@@ -199,7 +199,6 @@ signal_editor_changed (XmlSpecEditor *sped)
  out:
 	if (lerror) {
 		if (! sped->priv->info) {
-#if GTK_CHECK_VERSION (2,18,0)
 			sped->priv->info = gtk_info_bar_new ();
 			gtk_box_pack_start (GTK_BOX (sped), sped->priv->info, FALSE, FALSE, 0);
 			sped->priv->info_label = gtk_label_new ("");
@@ -207,10 +206,6 @@ signal_editor_changed (XmlSpecEditor *sped)
 			gtk_label_set_ellipsize (GTK_LABEL (sped->priv->info_label), PANGO_ELLIPSIZE_END);
 			gtk_container_add (GTK_CONTAINER (gtk_info_bar_get_content_area (GTK_INFO_BAR (sped->priv->info))),
 					   sped->priv->info_label);
-#else
-			sped->priv->info = gtk_label_new ("");
-			sped->priv->info_label = sped->priv->info;
-#endif
 			gtk_widget_show (sped->priv->info_label);
 		}
 		gchar *str;
