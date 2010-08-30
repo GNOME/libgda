87d6e09f2a20871b5eca0e44f6f27971190b746c
Robert Ancell <robert.ancell@canonical.com>
2010-08-30 15:48:40 +1000

    Fix introspection build

--- libgda/gda-tree-manager.h
+++ libgda/gda-tree-manager.h
@@ -62,8 +62,9 @@ struct _GdaTreeManagerClass {
 	 *
 	 * Returns: NULL if an error occurred, and @out_error is set to TRUE
 	 */
-	GdaTreeManagerNodesFunc update_children;
-
+	GSList *(*update_children) (GdaTreeManager *manager, GdaTreeNode *node,
+                                const GSList *children_nodes,
+                                gboolean *out_error, GError **error);
 
 	/*< private >*/
 	/* Padding for future expansion */
