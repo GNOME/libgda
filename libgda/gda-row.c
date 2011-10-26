bdbb4b1b82da0e37b707083b6b6a6d27e6a8618b
Vivien Malerba <malerba@gnome-db.org>
2011-10-26 17:12:49 +0200

    Have "make check" work
    removes bug too quickly introduced in commit #335f0d27fcea69989fcdbb2cbe7c2718a9a40fa9

--- libgda/gda-row.c
+++ libgda/gda-row.c
@@ -144,16 +144,12 @@ gda_row_set_property (GObject *object,
         row = GDA_ROW (object);
         if (row->priv) {
                 switch (param_id) {
-		case PROP_NB_VALUES: {
-			gint i;
+		case PROP_NB_VALUES:
 			g_return_if_fail (!row->priv->fields);
 
 			row->priv->nfields = g_value_get_int (value);
-			row->priv->fields = g_new0 (GValue, row->priv->nfields);
-			for (i = 0; i < row->priv->nfields; i++)
-				gda_value_set_null (& (row->priv->fields [i]));
+			row->priv->fields = g_new0 (GValue, row->priv->nfields);			
 			break;
-		}
 		default:
 			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
 			break;
@@ -276,7 +272,7 @@ void
 gda_row_invalidate_value_e (GdaRow *row, GValue *value, GError *error)
 {
 	gda_value_set_null (value);
-	G_VALUE_TYPE (value) = G_TYPE_INVALID;
+	G_VALUE_TYPE (value) = G_TYPE_NONE;
 	if (error) {
 		guint i;
 		if (! row->priv->errors)
@@ -345,7 +341,7 @@ gboolean
 gda_row_value_is_valid_e (GdaRow *row, GValue *value, GError **error)
 {
 	gboolean valid;
-	valid = (G_VALUE_TYPE (value) == G_TYPE_INVALID) ? FALSE : TRUE;
+	valid = (G_VALUE_TYPE (value) == G_TYPE_NONE) ? FALSE : TRUE;
 	if (!valid && row->priv->errors && error) {
 		guint i;
 		for (i = 0; i < row->priv->nfields; i++) {
