0bff4309e7ca0432fb4648a86839063e44a8618b
Pavlo Solntsev <p.sun.fun@gmail.com>
2018-05-22 23:47:05 -0500

    Initial interface for DDL module, code formating

--- libgda/gda-ddl-fkey.c
+++ libgda/gda-ddl-fkey.c
@@ -20,6 +20,7 @@
 #include "gda-ddl-fkey.h"
 #include <glib/gi18n-lib.h>
 #include <glib.h>
+#include "gda-ddl-buildable.h"
 
 G_DEFINE_QUARK (gda-ddl-fkey-error, gda_ddl_fkey_error)
 
@@ -38,7 +39,11 @@ typedef struct
   GdaDdlFkeyReferenceAction m_ondelete;
 }GdaDdlFkeyPrivate;
 
-G_DEFINE_TYPE_WITH_PRIVATE (GdaDdlFkey, gda_ddl_fkey, G_TYPE_OBJECT)
+static void gda_ddl_fkey_buildable_interface_init (GdaDdlBuildableInterface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (GdaDdlFkey, gda_ddl_fkey, G_TYPE_OBJECT,
+                         G_IMPLEMENT_INTERFACE (GDA_TYPE_DDL_BUILDABLE,
+                                                gda_ddl_fkey_buildable_interface_init))
 
 static const gchar *OnAction[] = {
     "NO ACTION",
@@ -94,6 +99,242 @@ gda_ddl_fkey_init (GdaDdlFkey *self)
 
   priv->m_onupdate = GDA_DDL_FKEY_NO_ACTION;
   priv->m_ondelete = GDA_DDL_FKEY_NO_ACTION;
+}
+
+/**
+ * gda_ddl_fkey_parse_node:
+ * @self: #GdaDdlFkey object
+ * @node: xml node to parse as #xmlNodePtr
+ * @error: #GError object to store error
+ *
+ * Use this method to populate corresponding #GdaDdlFkey object from xml node. Usually,
+ * this method is called from #GdaDdlCreator suring parsing the imput xml file.
+ *
+ * The corresponding DTD section suitable for parsing by this method should correspond
+ * th the following code:
+ *
+ * |[<!-- language="dtd" -->
+ * <!ELEMENT fkey (fk_field?)>
+ * <!ATTLIST fkey reftable IDREF #IMPLIED>
+ * <!ATTLIST fkey onupdate (RESTRICT|CASCADE|SET_NULL|NO_ACTION|SET_DEFAULT) #IMPLIED>
+ * <!ATTLIST fkey ondelete (RESTRICT|CASCADE|SET_NULL|NO_ACTION|SET_DEFAULT) #IMPLIED>
+ *
+ * <!ELEMENT fk_field (#PCDATA)>
+ * <!ATTLIST fk_field name      IDREF #REQUIRED>
+ * <!ATTLIST fk_field reffield  IDREF #REQUIRED>
+ * ]|
+ *
+ * Returns: %FALSE if an error occures, %TRUE otherwise
+ *
+ * Since: 6.0
+ */
+static gboolean
+gda_ddl_fkey_parse_node (GdaDdlBuildable *buildable,
+                         xmlNodePtr       node,
+                         GError         **error)
+{
+  GdaDdlFkey *self = GDA_DDL_FKEY (buildable);
+  g_return_val_if_fail (node, FALSE);
+  //    g_return_val_if_fail(!*error, FALSE);
+  /*
+   *    <fkey reftable="products" onupdate="NO_ACTION" ondelete="NO_ACTION">
+   *        <fk_field name="column_name" reffield="column_name"/>
+   *        <fk_field name="column_id" reffield="column_"/>
+   *    </fkey>
+   */
+  GdaDdlFkeyPrivate *priv = gda_ddl_fkey_get_instance_private (self);
+  xmlChar *prop = NULL;
+
+  prop = xmlGetProp (node,(xmlChar *)"reftable");
+
+  g_assert (prop); /* Bug with xml valdation */
+
+  priv->mp_ref_table = g_strdup ((gchar *)prop);
+  xmlFree (prop);
+  prop = NULL;
+
+  prop = xmlGetProp (node,(xmlChar *)"onupdate");
+
+  g_assert(prop);
+
+  priv->m_onupdate = GDA_DDL_FKEY_NO_ACTION;
+
+  for (guint i = 0; i < G_N_ELEMENTS(OnAction);i++) {
+      if (!g_strcmp0 (prop,OnAction[i]))
+        priv->m_onupdate = (GdaDdlFkeyReferenceAction)i;
+  }
+
+  xmlFree (prop);
+  prop = NULL;
+
+  prop = xmlGetProp (node,(xmlChar *)"ondelete");
+
+  g_assert(prop);
+
+  for (guint i = 0; i < G_N_ELEMENTS(OnAction);i++) {
+      if (!g_strcmp0 (prop,OnAction[i]))
+        priv->m_ondelete = (GdaDdlFkeyReferenceAction)i;
+  }
+
+  xmlFree (prop);
+  prop = NULL;
+
+  xmlChar *name = NULL;
+  xmlChar *reffield = NULL;
+
+  for (xmlNodePtr it = node->children; it; it = it->next) {
+      if (!g_strcmp0 ((gchar *)it->name, "fk_field")) {
+          name = xmlGetProp (it,(xmlChar *)"name");
+
+          g_assert(name);
+          priv->mp_field = g_list_append (priv->mp_field,
+                                          g_strdup ((const gchar *)name));
+          xmlFree (name);
+
+          reffield = xmlGetProp (it, (xmlChar *)"reffield");
+          g_assert(reffield);
+          priv->mp_ref_field = g_list_append (priv->mp_ref_field,
+                                              g_strdup ((const gchar *)reffield));
+          xmlFree (reffield);
+      } /* end of if */
+  } /* end of for loop */
+  return TRUE;
+}
+
+/**
+ * gda_ddl_fkey_write_xml:
+ * @self: An object #GdaDdlFkey
+ * @writer: An object to #xmlTextWriterPtr instance
+ * @error: A place to store error
+ *
+ * An appropriate encoding and version should be placed to the writer before
+ * it is used. This method will not add any header, just populate appropriate
+ * fields in the xml tree. The new xml block should be something like:
+ *
+ * |[<!-- language="xml" -->
+ * <fkey reftable="products" onupdate="NO_ACTION" ondelete="NO_ACTION">
+ *     <fk_field name="column_namefk1" reffield="column_name1"/>
+ *     <fk_field name="column_namefk2" reffield="column_name2"/>
+ *     <fk_field name="column_namefk3" reffield="column_name3"/>
+ * </fkey>
+ *]|
+ * Returns: TRUE if no error, FALSE otherwise
+ */
+static gboolean
+gda_ddl_fkey_write_node (GdaDdlBuildable  *buildable,
+                         xmlTextWriterPtr  writer,
+                         GError          **error)
+{
+  GdaDdlFkey *self = GDA_DDL_FKEY (buildable);
+  g_return_val_if_fail (writer, FALSE);
+
+  GdaDdlFkeyPrivate *priv = gda_ddl_fkey_get_instance_private (self);
+
+  int res = 0;
+
+  res = xmlTextWriterStartElement(writer, BAD_CAST "fkey");
+  if (res < 0) {
+      g_set_error (error,
+                   GDA_DDL_FKEY_ERROR,
+                   GDA_DDL_FKEY_ERROR_START_ELEMENT,
+                   _("Can't set start element <fkey> in xml tree\n"));
+      return FALSE;
+  }
+
+  res = xmlTextWriterWriteAttribute (writer,"reftable",
+                                     (xmlChar*)priv->mp_ref_table);
+
+  if (res < 0) {
+      g_set_error (error,
+                   GDA_DDL_FKEY_ERROR,
+                   GDA_DDL_FKEY_ERROR_ATTRIBUTE,
+                   _("Can't set reftable attribute to element <fkey>\n"));
+      return FALSE;
+  }
+
+  res = xmlTextWriterWriteAttribute (writer,"onupdate",
+                                     (xmlChar*)OnAction[priv->m_onupdate]);
+
+  if (res < 0) {
+      g_set_error (error,
+                   GDA_DDL_FKEY_ERROR,
+                   GDA_DDL_FKEY_ERROR_ATTRIBUTE,
+                   _("Can't set onupdate attribute to element <fkey>\n"));
+      return FALSE;
+  }
+
+  res = xmlTextWriterWriteAttribute (writer,"ondelete",
+                                     (xmlChar*)OnAction[priv->m_ondelete]);
+
+  if (res < 0) {
+      g_set_error (error,
+                   GDA_DDL_FKEY_ERROR,
+                   GDA_DDL_FKEY_ERROR_ATTRIBUTE,
+                   _("Can't set ondelete attribute to element <fkey>\n"));
+      return FALSE;
+  }
+
+  GList *it = priv->mp_field;
+  GList *jt = priv->mp_ref_field;
+
+  for (; it && jt; it = it->next, jt=jt->next ) {
+      res = xmlTextWriterStartElement(writer, BAD_CAST "fk_field");
+      if (res < 0) {
+          g_set_error (error,
+                       GDA_DDL_FKEY_ERROR,
+                       GDA_DDL_FKEY_ERROR_START_ELEMENT,
+                       _("Can't set start element <fk_field> in xml tree\n"));
+          return FALSE;
+      }
+
+      res = xmlTextWriterWriteAttribute (writer,"name",(xmlChar*)it->data);
+
+      if (res < 0) {
+          g_set_error (error,
+                       GDA_DDL_FKEY_ERROR,
+                       GDA_DDL_FKEY_ERROR_ATTRIBUTE,
+                       _("Can't set ondelete attribute to element <fk_field>\n"));
+          return FALSE;
+      }
+
+      res = xmlTextWriterWriteAttribute (writer,"reffield",(xmlChar*)jt->data);
+
+      if (res < 0) {
+          g_set_error (error,
+                       GDA_DDL_FKEY_ERROR,
+                       GDA_DDL_FKEY_ERROR_ATTRIBUTE,
+                       _("Can't set ondelete attribute to element <fk_field>\n"));
+          return FALSE;
+      }
+
+      res = xmlTextWriterEndElement (writer);
+
+      if (res < 0) {
+          g_set_error (error,
+                       GDA_DDL_FKEY_ERROR,
+                       GDA_DDL_FKEY_ERROR_END_ELEMENT,
+                       _("Can't close element <fk_field>\n"));
+          return FALSE;
+      }
+  }
+
+  res = xmlTextWriterEndElement (writer);
+
+  if (res < 0) {
+      g_set_error (error,
+                   GDA_DDL_FKEY_ERROR,
+                   GDA_DDL_FKEY_ERROR_END_ELEMENT,
+                   _("Can't close element <fkey>\n"));
+      return FALSE;
+  }
+
+  return TRUE;
+}
+static void
+gda_ddl_fkey_buildable_interface_init (GdaDdlBuildableInterface *iface)
+{
+    iface->parse_node = gda_ddl_fkey_parse_node;
+    iface->write_node = gda_ddl_fkey_write_node;
 }
 
 /**
@@ -281,105 +522,6 @@ gda_ddl_fkey_get_ref_field (GdaDdlFkey *self)
   return priv->mp_ref_field;
 }
 
-/**
- * gda_ddl_fkey_parse_node:
- * @self: #GdaDdlFkey object
- * @node: xml node to parse as #xmlNodePtr
- * @error: #GError object to store error
- *
- * Use this method to populate corresponding #GdaDdlFkey object from xml node. Usually,
- * this method is called from #GdaDdlCreator suring parsing the imput xml file.
- *
- * The corresponding DTD section suitable for parsing by this method should correspond
- * th the following code:
- *
- * |[<!-- language="dtd" -->
- * <!ELEMENT fkey (fk_field?)>
- * <!ATTLIST fkey reftable IDREF #IMPLIED>
- * <!ATTLIST fkey onupdate (RESTRICT|CASCADE|SET_NULL|NO_ACTION|SET_DEFAULT) #IMPLIED>
- * <!ATTLIST fkey ondelete (RESTRICT|CASCADE|SET_NULL|NO_ACTION|SET_DEFAULT) #IMPLIED>
- *
- * <!ELEMENT fk_field (#PCDATA)>
- * <!ATTLIST fk_field name	IDREF #REQUIRED>
- * <!ATTLIST fk_field reffield	IDREF #REQUIRED>
- * ]|
- *
- * Returns: %FALSE if an error occures, %TRUE otherwise
- *
- * Since: 6.0
- */
-gboolean
-gda_ddl_fkey_parse_node (GdaDdlFkey	*self,
-                         xmlNodePtr	node,
-                         GError     **error)
-{
-  g_return_val_if_fail (self, FALSE);
-  g_return_val_if_fail (node, FALSE);
-  //    g_return_val_if_fail(!*error, FALSE);
-  /*
-   *	<fkey reftable="products" onupdate="NO_ACTION" ondelete="NO_ACTION">
-   *	    <fk_field name="column_name" reffield="column_name"/>
-   *	    <fk_field name="column_id" reffield="column_"/>
-   *	</fkey>
-   */
-  GdaDdlFkeyPrivate *priv = gda_ddl_fkey_get_instance_private (self);
-  xmlChar *prop = NULL;
-
-  prop = xmlGetProp (node,(xmlChar *)"reftable");
-
-  g_assert (prop); /* Bug with xml valdation */
-
-  priv->mp_ref_table = g_strdup ((gchar *)prop);
-  xmlFree (prop);
-  prop = NULL;
-
-  prop = xmlGetProp (node,(xmlChar *)"onupdate");
-
-  g_assert(prop);
-
-  priv->m_onupdate = GDA_DDL_FKEY_NO_ACTION;
-
-  for (guint i = 0; i < G_N_ELEMENTS(OnAction);i++) {
-      if (!g_strcmp0 (prop,OnAction[i]))
-        priv->m_onupdate = (GdaDdlFkeyReferenceAction)i;
-  }
-
-  xmlFree (prop);
-  prop = NULL;
-
-  prop = xmlGetProp (node,(xmlChar *)"ondelete");
-
-  g_assert(prop);
-
-  for (guint i = 0; i < G_N_ELEMENTS(OnAction);i++) {
-      if (!g_strcmp0 (prop,OnAction[i]))
-        priv->m_ondelete = (GdaDdlFkeyReferenceAction)i;
-  }
-
-  xmlFree (prop);
-  prop = NULL;
-
-  xmlChar *name = NULL;
-  xmlChar *reffield = NULL;
-
-  for (xmlNodePtr it = node->children; it; it = it->next) {
-      if (!g_strcmp0 ((gchar *)it->name, "fk_field")) {
-          name = xmlGetProp (it,(xmlChar *)"name");
-
-          g_assert(name);
-          priv->mp_field = g_list_append (priv->mp_field,
-                                          g_strdup ((const gchar *)name));
-          xmlFree (name);
-
-          reffield = xmlGetProp (it, (xmlChar *)"reffield");
-          g_assert(reffield);
-          priv->mp_ref_field = g_list_append (priv->mp_ref_field,
-                                              g_strdup ((const gchar *)reffield));
-          xmlFree (reffield);
-      } /* end of if */
-  } /* end of for loop */
-  return TRUE;
-}
 
 /**
  * gda_ddl_fkey_free:
@@ -420,134 +562,5 @@ gda_ddl_fkey_set_field (GdaDdlFkey  *self,
   priv->mp_ref_field = g_list_append(priv->mp_ref_field,(gpointer)reffield);
 }
 
-/**
- * gda_ddl_fkey_write_xml:
- * @self: An object #GdaDdlFkey
- * @writer: An object to #xmlTextWriterPtr instance
- * @error: A place to store error
- *
- * An appropriate encoding and version should be placed to the writer before
- * it is used. This method will not add any header, just populate appropriate
- * fields in the xml tree. The new xml block should be something like:
- *
- * |[<!-- language="xml" -->
- * <fkey reftable="products" onupdate="NO_ACTION" ondelete="NO_ACTION">
- *     <fk_field name="column_namefk1" reffield="column_name1"/>
- *     <fk_field name="column_namefk2" reffield="column_name2"/>
- *     <fk_field name="column_namefk3" reffield="column_name3"/>
- * </fkey>
- *]|
- * Returns: TRUE if no error, FALSE otherwise
- */
-gboolean
-gda_ddl_fkey_write_xml (GdaDdlFkey  *self,
-                        xmlTextWriterPtr writer,
-                        GError     **error)
-{
-  g_return_val_if_fail (self, FALSE);
-  g_return_val_if_fail (writer, FALSE);
-
-  GdaDdlFkeyPrivate *priv = gda_ddl_fkey_get_instance_private (self);
-
-  int res = 0;
-
-  res = xmlTextWriterStartElement(writer, BAD_CAST "fkey");
-  if (res < 0) {
-      g_set_error (error,
-                   GDA_DDL_FKEY_ERROR,
-                   GDA_DDL_FKEY_ERROR_START_ELEMENT,
-                   _("Can't set start element <fkey> in xml tree\n"));
-      return FALSE;
-  }
-
-  res = xmlTextWriterWriteAttribute (writer,"reftable",
-                                     (xmlChar*)priv->mp_ref_table);
-
-  if (res < 0) {
-      g_set_error (error,
-                   GDA_DDL_FKEY_ERROR,
-                   GDA_DDL_FKEY_ERROR_ATTRIBUTE,
-                   _("Can't set reftable attribute to element <fkey>\n"));
-      return FALSE;
-  }
-
-  res = xmlTextWriterWriteAttribute (writer,"onupdate",
-                                     (xmlChar*)OnAction[priv->m_onupdate]);
-
-  if (res < 0) {
-      g_set_error (error,
-                   GDA_DDL_FKEY_ERROR,
-                   GDA_DDL_FKEY_ERROR_ATTRIBUTE,
-                   _("Can't set onupdate attribute to element <fkey>\n"));
-      return FALSE;
-  }
-
-  res = xmlTextWriterWriteAttribute (writer,"ondelete",
-                                     (xmlChar*)OnAction[priv->m_ondelete]);
-
-  if (res < 0) {
-      g_set_error (error,
-                   GDA_DDL_FKEY_ERROR,
-                   GDA_DDL_FKEY_ERROR_ATTRIBUTE,
-                   _("Can't set ondelete attribute to element <fkey>\n"));
-      return FALSE;
-  }
-
-  GList *it = priv->mp_field;
-  GList *jt = priv->mp_ref_field;
-
-  for (; it && jt; it = it->next, jt=jt->next ) {
-      res = xmlTextWriterStartElement(writer, BAD_CAST "fk_field");
-      if (res < 0) {
-          g_set_error (error,
-                       GDA_DDL_FKEY_ERROR,
-                       GDA_DDL_FKEY_ERROR_START_ELEMENT,
-                       _("Can't set start element <fk_field> in xml tree\n"));
-          return FALSE;
-      }
-
-      res = xmlTextWriterWriteAttribute (writer,"name",(xmlChar*)it->data);
-
-      if (res < 0) {
-          g_set_error (error,
-                       GDA_DDL_FKEY_ERROR,
-                       GDA_DDL_FKEY_ERROR_ATTRIBUTE,
-                       _("Can't set ondelete attribute to element <fk_field>\n"));
-          return FALSE;
-      }
-
-      res = xmlTextWriterWriteAttribute (writer,"reffield",(xmlChar*)jt->data);
-
-      if (res < 0) {
-          g_set_error (error,
-                       GDA_DDL_FKEY_ERROR,
-                       GDA_DDL_FKEY_ERROR_ATTRIBUTE,
-                       _("Can't set ondelete attribute to element <fk_field>\n"));
-          return FALSE;
-      }
-
-      res = xmlTextWriterEndElement (writer);
-
-      if (res < 0) {
-          g_set_error (error,
-                       GDA_DDL_FKEY_ERROR,
-                       GDA_DDL_FKEY_ERROR_END_ELEMENT,
-                       _("Can't close element <fk_field>\n"));
-          return FALSE;
-      }
-  }
-
-  res = xmlTextWriterEndElement (writer);
-
-  if (res < 0) {
-      g_set_error (error,
-                   GDA_DDL_FKEY_ERROR,
-                   GDA_DDL_FKEY_ERROR_END_ELEMENT,
-                   _("Can't close element <fkey>\n"));
-      return FALSE;
-  }
-
-  return TRUE;
-}
 
 
