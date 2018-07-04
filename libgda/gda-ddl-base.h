f675c23652a82f2f755706e840b2508f7fa3f9ca
Pavlo Solntsev <p.sun.fun@gmail.com>
2018-07-04 11:35:21 -0500

    DDL: formating and license corection

--- libgda/gda-ddl-base.h
+++ libgda/gda-ddl-base.h
@@ -1,19 +1,21 @@
 /* gda-ddl-base.h
  *
- * Copyright © 2018 Pavlo Solntsev <p.sun.fun@gmail.com>
+ * Copyright (C) 2018 Pavlo Solntsev <p.sun.fun@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
  */
 
 #ifndef GDA_DDL_BASE_H
@@ -30,32 +32,33 @@ G_DECLARE_DERIVABLE_TYPE (GdaDdlBase, gda_ddl_base, GDA, DDL_BASE, GObject)
 
 struct _GdaDdlBaseClass
 {
-	GObjectClass parent;
+  GObjectClass parent;
 };
 
-GdaDdlBase*		gda_ddl_base_new (void);
+GdaDdlBase*   gda_ddl_base_new (void);
 
-void			gda_ddl_base_set_names (GdaDdlBase *self,
-										const gchar *catalog,
-										const gchar *schema,
-										const gchar *name);
+void          gda_ddl_base_set_names     (GdaDdlBase *self,
+                                          const gchar *catalog,
+                                          const gchar *schema,
+                                          const gchar *name);
 
-const gchar*	gda_ddl_base_get_full_name	(GdaDdlBase *self);
-const gchar*	gda_ddl_base_get_name		(GdaDdlBase *self);
-void			gda_ddl_base_set_name		(GdaDdlBase *self,
-											 const gchar *name);
+const gchar*  gda_ddl_base_get_full_name  (GdaDdlBase *self);
+const gchar*  gda_ddl_base_get_name       (GdaDdlBase *self);
+void          gda_ddl_base_set_name       (GdaDdlBase *self,
+                                           const gchar *name);
 
-const gchar*	gda_ddl_base_get_catalog	(GdaDdlBase *self);
-void			gda_ddl_base_set_catalog	(GdaDdlBase  *self,
-											 const gchar *catalog);
+const gchar*  gda_ddl_base_get_catalog    (GdaDdlBase *self);
+void          gda_ddl_base_set_catalog    (GdaDdlBase  *self,
+                                           const gchar *catalog);
 
-const gchar*	gda_ddl_base_get_schema		(GdaDdlBase *self);
-void			gda_ddl_base_set_schema		(GdaDdlBase  *self,
-											 const gchar *schema);
+const gchar*  gda_ddl_base_get_schema     (GdaDdlBase *self);
+void          gda_ddl_base_set_schema     (GdaDdlBase  *self,
+                                           const gchar *schema);
 
-void			gda_ddl_base_free			(GdaDdlBase *self);
+void          gda_ddl_base_free           (GdaDdlBase *self);
 
-gint      gda_ddl_base_compare (GdaDdlBase *a, GdaDdlBase *b);
+gint          gda_ddl_base_compare        (GdaDdlBase *a, GdaDdlBase *b);
+
 #define GDA_BOOL_TO_STR(x) (x ? "TRUE" : "FALSE")
 G_END_DECLS
 
