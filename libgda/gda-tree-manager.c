87886cc2aef9d83cc6951d642e211555081eb876
Daniel Espinosa <esodan@gmail.com>
2018-07-10 16:16:13 -0500

    GI: update annotations for GdaSet and GdaTreeManager

--- libgda/gda-tree-manager.c
+++ libgda/gda-tree-manager.c
@@ -491,7 +491,7 @@ gda_tree_manager_set_node_create_func (GdaTreeManager *manager, GdaTreeManagerNo
 }
 
 /**
- * gda_tree_manager_get_node_create_func:
+ * gda_tree_manager_get_node_create_func: (skip)
  * @manager: a #GdaTreeManager tree manager object
  *
  * Get the function used by @manager when creating new #GdaTreeNode nodes
