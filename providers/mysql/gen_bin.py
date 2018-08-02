4384e03fdbbc63c925bfd709e8a11c4caa9431f0
Daniel Espinosa <esodan@gmail.com>
2018-08-02 12:09:51 -0500

    Meson: Added MySQL provider

---
+++ providers/mysql/gen_bin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Create a hander bin for providers.')
+parser.add_argument('source', help='file path to create provider hander bin from')
+parser.add_argument('-o', '--output', help='file output')
+
+args=parser.parse_args()
+dargs=vars(args)
+
+f = open(dargs['source'])
+s = str(f.read())
+s = s.replace('SQLITE','MYSQL')
+s = s.replace('sqlite','mysql')
+s = s.replace('Sqlite','Mysql')
+s = s.replace('SQLite','MySQL')
+with open(os.path.join(dargs['output']), 'w') as file:
+    file.write(s)
+
+
+
