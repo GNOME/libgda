f41e7a199f2d294b5181967ccecee49ca82c5ca1
Pavlo Solntsev <p.sun.fun@gmail.com>
2019-03-21 07:19:07 -0500

    DB: GdaDbIndex and GdaDbIndexField objects added
    New Objects added to simplify DDL operations with indexes.

---
+++ libgda/gda-db-index-field.c
@@ -0,0 +1,214 @@
+/* gda-db-index-index.c
+ *
+ * Copyright (C) 2019 Pavlo Solntsev <p.sun.fun@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+#include "gda-db-index-field.h"
+
+typedef struct
+{
+  GdaDbColumn *mColumn;
+  GdaDbIndexCollate mCollate;
+  GdaDbIndexSortOrder mSortOrder;
+} GdaDbIndexFieldPrivate;
+
+
+/**
+ * SECTION:gda-db-index-field
+ * @short_description: Object to represent table index
+ * @see_also: #GdaDbTable, #GdaDbCatalog
+ * @stability: Stable
+ * @include: libgda/libgda.h
+ *
+ * This object is a container for information needed to create an index for a table. After
+ * population with information, it should be passed to the #GdaDbIndex instance.
+ *
+ */
+
+G_DEFINE_TYPE_WITH_PRIVATE(GdaDbIndexField, gda_db_index_field, G_TYPE_OBJECT)
+
+/**
+ * gda_db_index_field_new:
+ *
+ * Create a new instance of #GdaDbIndexField
+ *
+ * Returns: (transfer full): A new instance of #GdaDbIndexField
+ *
+ * Since: 6.0
+ */
+GdaDbIndexField *
+gda_db_index_field_new (void)
+{
+  return g_object_new (GDA_TYPE_DB_INDEX_FIELD, NULL);
+}
+static void
+gda_db_index_field_dispose (GObject *object)
+{
+  GdaDbIndexField *self = GDA_DB_INDEX_FIELD (object);
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+
+  g_object_unref (priv->mColumn);
+
+  G_OBJECT_CLASS (gda_db_index_field_parent_class)->dispose (object);
+}
+
+static void
+gda_db_index_field_class_init (GdaDbIndexFieldClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = gda_db_index_field_dispose;
+}
+
+static void
+gda_db_index_field_init (GdaDbIndexField *self)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+
+  priv->mColumn = NULL;
+  priv->mCollate = GDA_DB_INDEX_COLLATE_BINARY;
+  priv->mSortOrder = GDA_DB_INDEX_SORT_ORDER_ASC;
+}
+
+/**
+ * gda_db_index_field_set_column:
+ * @self: an instance of #GdaDbIndexField
+ * @column: column to add index to
+ *
+ * Only full name will be extracted from @column.
+ *
+ * Since: 6.0
+ */
+void
+gda_db_index_field_set_column (GdaDbIndexField *self,
+                               GdaDbColumn *column)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+
+  if (priv->mColumn)
+    g_object_unref (priv->mColumn);
+
+  priv->mColumn = g_object_ref (column);
+}
+
+/**
+ * gda_db_index_field_get_column:
+ * @self: a #GdaDbIndexField instance
+ *
+ * Returns an active column that was asigned to #GdaDbIndexField instance
+ *
+ * Returns: (transfer none): A #GdaDbColumn where index should be added
+ *
+ * Since: 6.0
+ */
+GdaDbColumn *
+gda_db_index_field_get_column (GdaDbIndexField *self)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+  return priv->mColumn;
+}
+
+/**
+ * gda_db_index_field_set_collate:
+ * @self: instance of #GdaDbIndexField
+ * @collate: collate to set
+ *
+ * Since: 6.0
+ */
+void
+gda_db_index_field_set_collate (GdaDbIndexField *self,
+                                GdaDbIndexCollate collate)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+  priv->mCollate = collate;
+}
+
+/**
+ * gda_db_index_field_get_collate:
+ * @self: instance of #GdaDbIndexField
+ *
+ * Returns: Collate value
+ *
+ * Since: 6.0
+ */
+GdaDbIndexCollate
+gda_db_index_field_get_collate (GdaDbIndexField *self)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+  return priv->mCollate;
+}
+
+/**
+ * gda_db_index_field_set_sort_order:
+ * @self: object to use
+ * @sorder: sort order to set
+ *
+ * Since: 6.0
+ */
+void
+gda_db_index_field_set_sort_order (GdaDbIndexField *self,
+                                   GdaDbIndexSortOrder sorder)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+  priv->mSortOrder = sorder;
+}
+
+/**
+ * gda_db_index_field_get_sort_order:
+ * @self: object to use
+ *
+ * Returns: sort order as a #GdaDbIndexSortOrder object
+ *
+ * Since: 6.0
+ */
+GdaDbIndexSortOrder
+gda_db_index_field_get_sort_order (GdaDbIndexField *self)
+{
+  GdaDbIndexFieldPrivate *priv = gda_db_index_field_get_instance_private (self);
+  return priv->mSortOrder;
+}
+
+const gchar *
+gda_db_index_field_get_collate_str (GdaDbIndexField *field)
+{
+  g_return_val_if_fail (GDA_IS_DB_INDEX_FIELD(field), NULL);
+
+  GdaDbIndexCollate collate = gda_db_index_field_get_collate(field);
+
+  if (collate == GDA_DB_INDEX_COLLATE_BINARY)
+    return "BINARY";
+  else if (collate == GDA_DB_INDEX_COLLATE_NOCASE)
+    return "NOCASE";
+  else
+    return NULL;
+}
+
+const gchar *
+gda_db_index_field_get_sort_order_str (GdaDbIndexField *field)
+{
+  g_return_val_if_fail (GDA_IS_DB_INDEX_FIELD(field), NULL);
+
+  GdaDbIndexSortOrder sorder = gda_db_index_field_get_sort_order (field);
+
+  if (sorder == GDA_DB_INDEX_SORT_ORDER_ASC)
+    return "ASC";
+  else if (sorder == GDA_DB_INDEX_SORT_ORDER_DESC)
+    return "DESC";
+  else
+    return NULL;
+}
---
+++ libgda/gda-db-index.h
@@ -0,0 +1,73 @@
+/*
+ * gda-db-index.h
+ * Copyright (C) 2019 Pavlo Solntsev <p.sun.fun@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef GDA_DB_INDEX_H
+#define GDA_DB_INDEX_H
+
+#include <glib.h>
+#include <glib-object.h>
+#include "gda-db-base.h"
+#include "gda-db-index-field.h"
+
+G_BEGIN_DECLS
+
+#define GDA_TYPE_DB_INDEX (gda_db_index_get_type())
+
+G_DECLARE_DERIVABLE_TYPE (GdaDbIndex, gda_db_index, GDA, DB_INDEX, GdaDbBase)
+
+struct _GdaDbIndexClass
+{
+  GdaDbBaseClass parent_class;
+};
+
+/**
+ * GdaDbIndexError:
+ *
+ */
+typedef enum {
+    GDA_DB_INDEX_CONNECTION_NOT_OPENED,
+    GDA_DB_INDEX_SERVER_OPERATION
+} GdaDbIndexError;
+
+#define GDA_DB_INDEX_ERROR gda_db_index_error_quark()
+
+GQuark gda_db_index_error_quark(void);
+
+GdaDbIndex       *gda_db_index_new (void);
+
+void              gda_db_index_set_unique (GdaDbIndex *self, gboolean val);
+gboolean          gda_db_index_get_unique (GdaDbIndex *self);
+
+void              gda_db_index_append_field (GdaDbIndex *self, GdaDbIndexField *field);
+
+void              gda_db_index_remove_field (GdaDbIndex *self, const gchar *name);
+
+GSList           *gda_db_index_get_fields   (GdaDbIndex *self);
+
+gboolean          gda_db_index_drop (GdaDbIndex *self,
+                                     GdaConnection *cnc,
+                                     gboolean ifexists,
+                                     GError **error);
+
+G_END_DECLS
+
+#endif /* end of include guard: GDA-DB-INDEX_H */
+
+
+
