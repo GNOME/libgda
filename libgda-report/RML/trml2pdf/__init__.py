bb2e1f3867d360d510437a42cc0a6b8ca1b8dc6c
Pavlo Solntsev <p.sun.fun@gmail.com>
2019-11-02 16:35:03 -0500

    libgda-report: Code ported to Python3

--- libgda-report/RML/trml2pdf/__init__.py
+++ libgda-report/RML/trml2pdf/__init__.py
@@ -15,4 +15,4 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from trml2pdf import parseString
+from .trml2pdf import parseString
