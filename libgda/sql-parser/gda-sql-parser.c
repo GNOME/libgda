1554ecf6748776e662d3bf978b545da29024532a
Daniel Espinosa <esodan@gmail.com>
2019-11-29 23:16:31 -0600

    GdaParser: remove spaces and enters before next statement
    Batch parsing, should remove spacesa and enters before
    return next statement.
    
    Fix issue #203

--- libgda/sql-parser/gda-sql-parser.c
+++ libgda/sql-parser/gda-sql-parser.c
@@ -587,6 +587,9 @@ gda_sql_parser_parse_string (GdaSqlParser *parser, const gchar *sql, const gchar
 		if (* priv->context->next_token_start) {
 			gint i = priv->context->next_token_start - priv->sql;
 			*remain = sql + i;
+			while (**remain == ' ' || **remain == '\n') {
+				*remain = sql + (++i);
+			}
 		}
 	}
 
